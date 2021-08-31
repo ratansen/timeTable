6ac4ff01257dc431075faebdbd9e4c04c3045281
Ratan-Sen <ratansen.iitg@gmail.com>
2021-08-31 17:40:43 +0530

    first commit

--- timetable/views.py
+++ timetable/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.messages.api import error
+from django.http import HttpResponseRedirect, response
+from django.db.models import query
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
