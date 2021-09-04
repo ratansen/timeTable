0ab6d46b8cafb8523bd3532a3815443a76b9381c
Ratan-Sen <ratansen.iitg@gmail.com>
2021-09-04 19:50:06 +0530

    deploy setup

--- ttproject/settings.py
+++ ttproject/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
-
+import dj_database_url
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -14,7 +15,7 @@ SECRET_KEY = 'django-insecure-6nh(e$-=l&6(f)k%zp)trikddcniq^6x8g50v^=@cqz6-fq^7k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,3 +116,4 @@ STATICFILES_DIRS = [STATIC_DIR,]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
