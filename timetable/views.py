27e6474a04d2b4f0b721873cf5dade646305e6f5
Akshay Khandelwal <39028584+akshaykhandelwal0710@users.noreply.github.com>
2021-10-03 16:46:03 +0530

    admin login implemented

--- timetable/views.py
+++ timetable/views.py
@@ -1,3 +1,4 @@
+from typing import NoReturn
 from timetable.models import Timetable
 from django.shortcuts import render
 from django.contrib.messages.api import error
@@ -5,7 +6,8 @@ from django.http import HttpResponseRedirect, response
 from django.db.models import query
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate
+from django.contrib import auth
 from django.contrib import messages
 
 days=['mon','tue','wed','thur','fri']
@@ -30,7 +32,26 @@ def showtable(request):
 
 
 def adminpanel(request):
-    return render(request,'admin.html')
+    if (request.user.is_authenticated):
+        return render(request,'admin.html')
+    else:
+        return redirect('/login')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def login(request):
+    if (request.method == "POST"):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+        return redirect('/adminpanel')
+    else:
+        return render(request, 'login.html')
 
 def submit(request):
     lower=int(request.POST['lower'])
@@ -82,4 +103,3 @@ def submit(request):
             fri_4_5 = getslots[ 39 ],
         )
     return redirect('/adminpanel')
-
--- ttproject/urls.py
+++ ttproject/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',include('timetable.urls')),
     path('showtable/',include('timetable.urls')),
     path('adminpanel/',include('timetable.urls')),
+    path('login/', include('timetable.urls')),
     path('submit/',include('timetable.urls')),
-
+    path('logout/', include('timetable.urls')),
 ]
